c9d301769664a17b5fee6dd5c7725247e3081f1a
Minami Sato <minami10076@gmail.com>
2023-03-06 22:07:23 -0800

    Minami lab-6 submission

--- js/lab6-1.js
+++ js/lab6-1.js
@@ -1,2 +1,49 @@
 /* jQuery and JavaScript code here for lab6-1.html */
+window.addEventListener('load', () => {
+  $("#pageTitle").text('Lab 6 – DOM Manipulation with jQuery');
 
+  $('#msgArea').val('My class is ' + $('#msgArea').attr('class'));
+
+  $('button:contains("View details")').css('background-color', 'red');
+
+  $('body').css('background-color', 'ivory');
+
+  $('.center-icons').each((index, element) => {
+    $(element).addClass('selected');
+  });
+
+  $('.panel').on('click', () => {
+    $('span#message').text("You clicked in this panel");
+  });
+
+  $('.panel').on('mousemove', (event) =>{
+    $('span#message').text("x =" + event.pageX + " y =" + event.pageY);
+  });
+
+  $('.panel').on('mouseleave', () => {
+    $('span#message').text("The mouse has left.");
+  });
+
+  console.log($('#stories img'))
+  $('#stories img').each((index, element) => {
+    $(element).on('mouseover', (event) => {
+      var alt = $(element).attr('alt'); 
+      var src = $(element).attr('src');         
+      var newsrc = src.replace("small","medium");
+
+      $('body').append('<div id=\"preview\"><img src=\"' + newsrc + '\"/><p>' + alt + '</p></div>');
+      $('#preview').css('position', 'absolute');
+      $('#preview').css('left', event.pageX);
+      $('#preview').css('top', event.pageY);
+      $('#preview').fadeIn();
+      
+      $(element).addClass('gray');
+    });
+
+    $(element).on('mouseleave', (event) => {
+      $('#preview').fadeOut();
+      $('#preview').remove()
+      $(element).removeClass('gray');
+    })
+  });
+});
